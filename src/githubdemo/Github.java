845360e6c63641b8b734f464246e7230775ba977
anjal <anjal@DESKTOP-23UM2O0>
2021-10-19 14:48:23 +0530

    first project

---
+++ src/githubdemo/Github.java
@@ -0,0 +1,17 @@
+package githubdemo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names= new String[] {"anjali,sai,jaya"};
+	    List<String> namelist= new ArrayList<String>(Arrays.asList(names));
+	    System.out.println(namelist);
+
+	}
+
+}
